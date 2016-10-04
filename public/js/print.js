ca80b1e1364bf616fb80fdfdebc3f15305ac8b44
pratimasakinala <pratimasakinala13@gmail.com>
2016-10-04 13:50:55 -0700

    customer name, order name, time displayed at top

--- public/js/print.js
+++ public/js/print.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  function print(butterfish) {
+  function print(order) {
     var builder = new StarWebPrintBuilder();
     var request = '';
     var seatValue = '';
@@ -12,7 +12,7 @@
 
     var trader = new StarWebPrintTrader({url:url, papertype:papertype});
 
-    trader.onReceive = function (response) {
+    /*trader.onReceive = function (response) {
 
       var msg = '- onReceive -\n\n';
       msg += 'TraderSuccess : [ ' + response.traderSuccess + ' ]\n';
@@ -31,7 +31,7 @@
 
       msg += '\tEtbCounter = ' + trader.extractionEtbCounter({traderStatus:response.traderStatus}).toString() + ' ]\n';
       alert(msg);
-    }
+    }*/
 
     trader.onError = function (response) {
       var msg = '- onError -\n\n';
@@ -46,59 +46,62 @@
     //  request += builder.createRuledLineElement({thickness: 'medium'});
     //  request += '\n';
 
-    //  var currentOrder = 'Order#: ' + butterfish.orderNo;
+    //  var currentOrder = 'Order#: ' + order.orderNo;
     //  request = createRequestTextElement(request, currentOrder);
+      seatValue = order.user_id.firstName + ' ' + order.user_id.lastName;
+      request = createRequestTextElement(request, seatValue);
 
-      for(var i=0; i<butterfish.seats.length; i++){
+      if(order.name !== ''){
+        request = createRequestTextElement(request, order.name);
+      }
+      
+      let time = new Date();
+      let hours = time.getHours();
+      let ampm = hours > 12 ? ' PM' : ' AM';
+      hours = hours % 12;
+      hours = hours ? hours : 12;
+      let currentTime = hours + ':' + time.getMinutes() + ampm;
+      request = createRequestTextElement(request, currentTime);
+
+      for(var i=0; i<order.seats.length; i++){
         request += builder.createRuledLineElement({thickness: 'medium'});
         request = createRequestTextElement(request, 'Seat ' + (i + 1));
 
-        for(var key in butterfish.seats[i]){
+        for(var key in order.seats[i]){
+          let double_protein = '';
           if(key === 'double_protein'){
-            var double_protein = butterfish.seats[i].double_protein;
+            double_protein = order.seats[i].double_protein;
             if(double_protein){
               request = createRequestTextElement(request, 'Double Protein');
             }
-          }else{
-            if(key === 'selected_items'){
-              var storage = '';
-              for (var j=0; j<butterfish.seats[i].selected_items.length; j++) {
-                var cat = butterfish.seats[i].selected_items[j].category.name;
-                if (storage !== cat) {
-                  request = createRequestTextElement(request, capitalize(butterfish.seats[i].selected_items[j].category.name) + ':');
-                  storage = butterfish.seats[i].selected_items[j].category.name;
-                }
-
-                var variation = '';
-                if (butterfish.seats[i].selected_items[j].category.name === 'beverages' ||
-                  butterfish.seats[i].selected_items[j].category.name === 'proteins' ||
-                  butterfish.seats[i].selected_items[j].category.name === 'signature_bowls')
-                    variation = butterfish.seats[i].selected_items[j].variation.name + ' ';
-
-                var multiplier = '';
-                var group = _.groupBy(butterfish.seats[i].selected_items, 'name')
-                var quantity = group[butterfish.seats[i].selected_items[j].name].length;
-                quantity === 1 ? multiplier = '' : multiplier = quantity.toString() + 'x ';
-
-                request = createRequestTextElement(request, '  ' + multiplier + capitalize(variation) + capitalize(butterfish.seats[i].selected_items[j].name));
+          }else if(key === 'selected_items'){
+            var storage = '';
+            for (var j=0; j<order.seats[i].selected_items.length; j++) {
+              var cat = order.seats[i].selected_items[j].category.name;
+              if (storage !== cat) {
+                request = createRequestTextElement(request, capitalize(order.seats[i].selected_items[j].category.name) + ':');
+                storage = order.seats[i].selected_items[j].category.name;
               }
-            } else {
-              if(key === 'special_instructions'){
-                request = createRequestTextElement(request, 'Special Instructions: \n  ' + capitalize(butterfish.seats[i].special_instructions));
-              }
+
+              var variation = '';
+              if (order.seats[i].selected_items[j].category.name == 'Beverages' ||
+                  order.seats[i].selected_items[j].category.name == 'Proteins' ||
+                  order.seats[i].selected_items[j].category.name == 'Signature Bowls')
+                  variation = order.seats[i].selected_items[j].variation.name + ' ';
+
+              var multiplier = '';
+              var group = _.groupBy(order.seats[i].selected_items, 'name')
+              var quantity = group[order.seats[i].selected_items[j].name].length;
+              quantity === 1 ? multiplier = '' : multiplier = quantity.toString() + 'x ';
+
+              request = createRequestTextElement(request, '  ' + multiplier + capitalize(variation) + capitalize(order.seats[i].selected_items[j].name));
+            }
+          }else if(key === 'special_instructions'){
+            if(order.seats[i].special_instructions !== ''){
+              request = createRequestTextElement(request, 'Special Instructions: \n  ' + capitalize(order.seats[i].special_instructions));
             }
           }
         }
-      }
-
-      function capitalize(name){
-       name = name.split(' ');
-       for(var a=0; a<name.length; a++){
-         name[a] = name[a].charAt(0).toUpperCase()
-                 + name[a].substring(1, name[a].length).toLowerCase();
-       }
-       name = name.toString().replace(/,/g, ' ');
-       return name;
       }
 
       request += '\n';
@@ -106,10 +109,20 @@
       request += builder.createRuledLineElement({thickness: 'medium'});
       request += builder.createFeedElement({line: 2});
       request += builder.createCutPaperElement({type: 'partial'});
-      trader.sendMessage({request:request});
+      //trader.sendMessage({request:request});
     }
     catch (e) {
       alert(e.message);
+    }
+
+    function capitalize(name){
+      name = name.split(' ');
+      for(var a=0; a<name.length; a++){
+        name[a] = name[a].charAt(0).toUpperCase()
+                + name[a].substring(1, name[a].length).toLowerCase();
+      }
+      name = name.toString().replace(/,/g, ' ');
+      return name;
     }
 
     function createRequestTextElement(request, seatValue){
@@ -122,14 +135,13 @@
         invert: false,
         linespace: 32,
         width: 1,
-        height: 2,
+        height: 1,
         font: 'font_a',
         underline: false,
         data: seatValue.toString() +'\n'
       });
       return request;
     }
-
   }
 
   Orders.registerOrderNotification(print);
