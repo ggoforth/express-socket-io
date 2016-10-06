48f7fbcf8c14d651447451da2b300dd497b4565b
Greg Goforth <greg.goforth@gmail.com>
2016-10-06 15:35:29 -0700

    adds bowl icon

--- public/js/app.js
+++ public/js/app.js
@@ -88,7 +88,7 @@
   function buildSeatHTML(seat) {
     var $cont = $('<div></div>'),
       $seat = $('<ul></ul>').addClass('seat'),
-      $order = $('<li><div class="bowl-size"><div class="bowl-size-inner">' + _.capitalize(findBowlSize(seat).slice(0, 1)) + '</div></div></li>'),
+      $order = $('<li><div class="bowl-size"><div class="bowl-size-inner"></div></div><span class="bowl-size-text">' + _.capitalize(findBowlSize(seat)) + ' Bowl</span> </li>'),
       $items = $('<ul></ul>').addClass('items'),
       items = _.groupBy(seat.selected_items, 'category.name');
     
