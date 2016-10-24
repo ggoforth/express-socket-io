a0844ddab1f67c49598593d0f1f58f5ca502e9d6
pratimasakinala <pratimasakinala13@gmail.com>
2016-10-24 14:00:59 -0700

    remove removeItem

--- public/js/orders.js
+++ public/js/orders.js
@@ -22,7 +22,6 @@
    * the order object from the server.
    */
   socket.on('newOrder', function (order) {
-    sessionStorage.removeItem('is_reloaded');
     Orders.storeOrder(order);
     Orders.runOrderNotifications(order);
   });
